37bef194ba7a8b10c86dba2a8450b6811b833bbc
Uthman Yusuf <yusufuthman57@egmail.com>
2022-01-08 08:02:54 +0100

    created a staging table for payments named stg_payments and renamed a few columns for readability

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,7 +1,8 @@
-SELECT "ID",
+SELECT "ID" as payment_id,
 	   "ORDERID" as order_id,
-	   "PAYMENTMETHOD",
+	   "PAYMENTMETHOD" as payment_method,
 	   "STATUS", 
-	   "AMOUNT",
-	   "CREATED"
+       --Amount is in cents so we divide by 100 to get the dollar value
+	   "AMOUNT"/100 as amount,
+	   "CREATED" as created_at
 	FROM public.stripe_payments
