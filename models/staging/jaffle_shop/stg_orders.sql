f09119e9a5940b5391d57328496b7ee42b9d6b13
Uthman Yusuf <yusufuthman57@egmail.com>
2022-01-08 07:56:16 +0100

    created a staging table for payments named stg_payments

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,6 +1,6 @@
 select
     "ID" as order_id,
     "USER_ID" as customer_id,
-    "ORDER_DATE",
-    "STATUS"
+    "ORDER_DATE" as order_date,
+    "STATUS" as status
 from public.jaffle_shop_orders
