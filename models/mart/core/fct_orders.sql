d530c86a9702f7fb6a4a0dc3ceebc15bd8382e8b
Uthman Yusuf <yusufuthman57@egmail.com>
2022-01-08 08:00:59 +0100

    refactored fct_orders to pick only successful or completed orders only

--- models/mart/core/fct_orders.sql
+++ models/mart/core/fct_orders.sql
@@ -4,13 +4,21 @@ with orders as (
 payments as (
     select * from {{ ref('stg_payments') }}
 ),
+order_payments as (
+    select 
+        order_id,
+        sum (case when "STATUS" = 'success' then amount else 0 end) as amount
+    from payments
+    GROUP BY order_id
+),
 final as (
     select 
         orders.order_id,
         orders.customer_id,
-        payments."AMOUNT"
+        orders.order_date,
+        coalesce(order_payments.amount,0) as amount
     FROM orders 
-    LEFT join payments
-    on orders.order_id  = payments.order_id
+    LEFT join order_payments
+    on orders.order_id  = order_payments.order_id
 )
 select * from final
