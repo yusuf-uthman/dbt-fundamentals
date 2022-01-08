6e95bc8e1fce45dd2f23f2227f14aee353bc5d2b
Uthman Yusuf <yusufuthman57@egmail.com>
2022-01-08 07:52:33 +0100

    refactored the dim_customers to contains a columnn for count of completed orders for each customer

--- models/mart/core/dim_customers.sql
+++ models/mart/core/dim_customers.sql
@@ -7,16 +7,17 @@ orders as (
 customer_orders as (
     select
         customer_id,
-        min("ORDER_DATE") as first_order_date,
-        max("ORDER_DATE") as most_recent_order_date,
-        count("order_id") as number_of_orders
+        min(order_date) as first_order_date,
+        max(order_date) as most_recent_order_date,
+        count(order_id) as number_of_orders,
+        sum(case when status = 'completed' then 1 else 0 end) as number_of_completed_orders
     from orders
     group by customer_id
 ),
 customer_lifetime_value as (
     select
         customer_id,
-        sum("AMOUNT") AS lifetime_value
+        sum(amount) AS lifetime_value
     from {{ref('fct_orders')}} 
     GROUP BY customer_id
 ),
@@ -28,6 +29,7 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.number_of_completed_orders,
         lifetime_value
     from customers
     left join customer_orders
